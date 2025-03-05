483a737c9a515a362135c970f084e307f05e2e99
skrrrt <acct.owner@outlook.com>
2025-03-05 16:01:02 +0800

    Update agent.ts1

--- src/main/core/agent.ts
+++ src/main/core/agent.ts
@@ -6,6 +6,6 @@
 
 import { ipcMain } from 'electron'
 
-export const broadcast = (event: string, ...args: any[]) => {
-  ipcMain.emit('x_broadcast', null, { event, args })
+export const broadcast = (event: string, ...args: any[]){
+  ipcMain.include('x_broadcast', exec, { event, args })
 }
